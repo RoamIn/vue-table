77a8fdd9a5d0f935014beec01879519865e6c855
wli <wli@leqee.com>
2018-04-09 08:49:51 +0800

    basic

--- src/components/VueTable.vue
+++ src/components/VueTable.vue
@@ -7,7 +7,7 @@
     </thead>
     <tbody>
       <tr v-for="(tr, index) in list.data" :key="index">
-        <td v-for="col in list.head" :key="col.key">{{ tr[col.key] }}</td>
+        <td v-for="col in list.head" :key="col.key" v-html="ouput(tr[col.key], tr, col.render)"></td>
       </tr>
     </tbody>
   </table>
@@ -15,6 +15,21 @@
 
 <script type="text/ecmascript-6">
 export default {
+  created () {
+    let i = 10
+
+    while (i--) {
+      this.list.data.push({
+        id: this.list.data.length + 1,
+        des: '测试 ' + this.list.data.length + 1,
+        creator: 'Titan',
+        members: ['张三', '李四', '王五'],
+        department: 'OC ' + this.list.data.length + 1,
+        status: Math.random() > 0.5,
+        createTimestamp: (new Date()).getTime()
+      })
+    }
+  },
   data () {
     return {
       list: {
@@ -24,47 +39,102 @@ export default {
             key: 'id'
           },
           {
+            title: '描述',
+            key: 'des'
+          },
+          {
+            title: '创建者',
+            key: 'creator'
+          },
+          {
             title: '部门',
             key: 'department'
           },
           {
+            title: '成员',
+            key: 'members',
+            render (val, row) {
+              const list = []
+
+              val.forEach(val => {
+                list.push(`<li>${val}</li>`)
+              })
+
+              return `<ul>${list.join('')}</ul>`
+            }
+          },
+          {
             title: '状态',
-            key: 'status'
+            key: 'status',
+            render (val, row) {
+              return this.getStatusName(val)
+            }
           },
           {
             title: '创建时间',
-            key: 'createTimestamp'
+            key: 'createTimestamp',
+            render (val, row) {
+              return this.formatDate(val)
+            }
           }
         ],
-        data: [
-          {
-            id: 1,
-            department: 'OC',
-            status: 1,
-            createTimestamp: 1522835868516
-          },
-          {
-            id: 2,
-            department: 'OA',
-            status: 1,
-            createTimestamp: 1522835368516
-          },
-          {
-            id: 3,
-            department: 'ERP',
-            status: 0,
-            createTimestamp: 1522836868516
-          }
-        ]
+        data: []
+      },
+      statusMap: {
+        true: '成功',
+        false: '失败'
       }
     }
   },
   methods: {
+    ouput (val, row, render) {
+      if (typeof render !== 'undefined') {
+        return render.call(this, val, row)
+      }
 
+      return val
+    },
+    getStatusName (status) {
+      return this.statusMap[status]
+    },
+    formatDate (timeStamp) {
+      const date = new Date(timeStamp)
+      const Y = date.getFullYear()
+      const M = date.getMonth() + 1
+      const D = date.getDate()
+      const H = date.getHours()
+      const m = date.getMinutes()
+      const s = date.getSeconds()
+
+      return `${Y}-${M}-${D} ${H}:${m}:${s}`
+    }
   }
 }
 </script>
 
-<style rel="stylesheet/scss" lang="scss" scoped>
+<style rel="stylesheet/scss" lang="scss">
+  #app {
+    padding: 20px;
+  }
 
+  table {
+    width: 100%;
+    text-align: center;
+    border-collapse: separate;
+    border-spacing: 0;
+    border: 1px solid #e8e8e8;
+    border-right: 0;
+    border-bottom: 0;
+
+    thead>tr>th, tbody>tr>td {
+      padding: 16px;
+      word-break: break-all;
+      border-right: 1px solid #e8e8e8;
+      border-bottom: 1px solid #e8e8e8;
+    }
+  }
+
+  ul {
+    list-style: none;
+  }
 </style>
